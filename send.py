459195c0255298ff96bd5de3d8acfb5d2f663828
Josh Nichols <technicalpickles@github.com>
2016-12-04 21:37:37 -0500

    sendingggg

--- send.py
+++ send.py
@@ -2,11 +2,25 @@ import broadlink
 from IPython import embed
 import time
 import yaml
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(prog='PROG')
+parser.add_argument('thing')
+parser.add_argument('state', choices=["on", "off"])
+args = parser.parse_args()
+
 
 with open('codes.yaml', 'r') as file:
     data = yaml.load(file)
 
+thing = data[args.thing]
+if args.state == 'on':
+    packet = thing[True]
+elif args.state == 'off':
+    packet = thing[False]
+
 devices = broadlink.discover(timeout=5)
 device = devices[0]
 device.auth()
-embed()
+device.send_data(packet)
