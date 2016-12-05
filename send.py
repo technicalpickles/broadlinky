2448e83ab0d684d72ad4177028b6c396eebdefcb
Josh Nichols <technicalpickles@github.com>
2016-12-05 17:31:40 -0500

    Fix execing, and fix relative path

--- send.py
+++ send.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python3
 from IPython import embed
 import argparse
 import broadlink
 import yaml
+import os
 
 parser = argparse.ArgumentParser(prog='PROG')
 parser.add_argument('thing')
 parser.add_argument('state', choices=["on", "off"])
 args = parser.parse_args()
 
+codes_path = os.path.dirname(os.path.abspath(__file__)) + '/codes.yaml'
 
-with open('codes.yaml', 'r') as file:
+with open(codes_path, 'r') as file:
     data = yaml.load(file)
 
 thing = data[args.thing]
