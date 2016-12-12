cf963149462c3a967b62bf568448b98f2da4dd8e
Josh Nichols <technicalpickles@github.com>
2016-12-12 09:51:32 -0500

    support digits in file

--- send.py
+++ send.py
@@ -4,13 +4,14 @@ import argparse
 import broadlink
 import yaml
 import os
+import re
 
 parser = argparse.ArgumentParser(prog='PROG')
 parser.add_argument('thing')
-parser.add_argument('state', choices=["on", "off"])
+parser.add_argument('state')
 args = parser.parse_args()
 
-codes_path = os.path.dirname(os.path.abspath(__file__)) + '/codes.yaml'
+codes_path = os.path.dirname(os.path.abspath(__file__)) + '/devices.yaml'
 
 with open(codes_path, 'r') as file:
     code_data = yaml.load(file)
@@ -20,6 +21,10 @@ if args.state == 'on':
     packet = thing[True]
 elif args.state == 'off':
     packet = thing[False]
+elif re.search('^\d+$', args.state):
+    packet = thing[int(args.state)]
+else:
+    packet = thing[args.state]
 
 devices = broadlink.discover(timeout=5)
 device = devices[0]
